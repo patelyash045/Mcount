3c8ffacde265ac02fcab0528044e5825f5406964
Yash <patelyash045@gmail.com>
2021-10-25 18:46:40 +0530

    Add the click to AddGroup Activity Added

--- app/src/main/java/com/yash/mcount/MainActivity.kt
+++ app/src/main/java/com/yash/mcount/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.yash.mcount
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yash.mcount.R
+import com.yash.mcount.activity.AddGroupActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fabCreate: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fabCreate = findViewById(R.id.fabCreateGroup)
+
+        fabCreate.setOnClickListener {
+            val intent = Intent(this, AddGroupActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
