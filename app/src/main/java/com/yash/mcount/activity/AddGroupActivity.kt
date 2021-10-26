23c7deda86b72acea477be09c59d3aa612ca3354
Yash <patelyash045@gmail.com>
2021-10-26 10:38:39 +0530

    Add the click to AddGroup Activity Added

--- app/src/main/java/com/yash/mcount/activity/AddGroupActivity.kt
+++ app/src/main/java/com/yash/mcount/activity/AddGroupActivity.kt
@@ -8,5 +8,7 @@ class AddGroupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_group)
+
+
     }
 }
